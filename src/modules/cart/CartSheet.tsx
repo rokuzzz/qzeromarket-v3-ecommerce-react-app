54cdca8e8805651654314565711e673add078de3
Roman Kuzero <rombeldan@gmail.com>
2024-09-19 05:28:33 +0300

    feat: implement functionality to place an order

--- src/modules/cart/CartSheet.tsx
+++ src/modules/cart/CartSheet.tsx
@@ -11,15 +11,74 @@ import { Separator } from '../shared/components/ui/separator';
 import { Button } from '../shared/components/ui/button';
 import { useCart } from '../cart/context/cartContext';
 import CartItem from './components/CartItem';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateOrder } from '../users/api/orderApi';
+import { useUser } from '../users/context/userContext';
+import { useToast } from '../shared/hooks/use-toast';
 
 const CartSheet = () => {
-  const { cartItems, productsMap, isLoading, error, clearCart } = useCart();
+  const {
+    cartItems,
+    productsMap,
+    isLoading,
+    error,
+    clearCart,
+    refetchCartItems,
+  } = useCart();
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const { toast } = useToast();
 
   const totalAmount = cartItems.reduce((total, item) => {
     const product = productsMap[item.productId];
     return product ? total + product.price * item.quantity : total;
   }, 0);
+
+  const createOrder = useCreateOrder({
+    onSuccess: () => {
+      refetchCartItems();
+      toast({
+        title: 'Order Placed',
+        description: 'Your order has been placed successfully.',
+        variant: 'default',
+      });
+      navigate('/user-profile');
+    },
+    onError: (error: any) => {
+      console.error('Failed to place order', error);
+      toast({
+        title: 'Error',
+        description: 'Failed to place order. Please try again.',
+        variant: 'destructive',
+      });
+    },
+  });
+
+  const handlePlaceOrder = () => {
+    if (!user) {
+      toast({
+        title: 'Login Required',
+        description: 'Please log in to place an order.',
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    if (cartItems.length === 0) {
+      toast({
+        title: 'Cart is Empty',
+        description: 'Add items to your cart before placing an order.',
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    const orderData = {
+      userId: user.id,
+    };
+
+    createOrder.mutate(orderData);
+  };
 
   return (
     <Sheet>
@@ -44,7 +103,7 @@ const CartSheet = () => {
             ) : error ? (
               <p className='text-red-500'>Error loading cart items.</p>
             ) : cartItems.length === 0 ? (
-              <></>
+              <p>Your cart is empty.</p>
             ) : (
               cartItems.map((item) => (
                 <CartItem key={item.id} cartItem={item} />
@@ -56,16 +115,26 @@ const CartSheet = () => {
         <div className='mt-auto pt-4'>
           <Separator />
           {cartItems.length > 0 ? (
-            <div className='flex justify-between items-center py-2'>
+            <>
+              <div className='flex justify-between items-center py-2'>
+                <Button
+                  variant='link'
+                  onClick={clearCart}
+                  className='text-red-500 p-0'
+                >
+                  Clear Cart
+                </Button>
+                <p className='font-bold'>Total: €{totalAmount.toFixed(2)}</p>
+              </div>
               <Button
-                variant='link'
-                onClick={clearCart}
-                className='text-red-500 p-0'
+                className='w-full uppercase mt-2'
+                variant={'default'}
+                size={'lg'}
+                onClick={handlePlaceOrder}
               >
-                Clear Cart
+                Place Order
               </Button>
-              <p className='font-bold'>Total: €{totalAmount.toFixed(2)}</p>
-            </div>
+            </>
           ) : (
             <Button
               className='w-full uppercase'
